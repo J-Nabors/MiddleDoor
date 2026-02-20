a8200f7f7cb2954834ea5ae785c4cd6142f0539f
John Nabors <johnhnabors@gmail.com>
2026-02-20 16:21:10 -0600

    Fixed hardcoding in MergeCensusYears
    I had initially not set up the repository correctlly, so now this allows for the data to live separately from the code.

---
+++ config.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+CENSUS_DATA_ROOT = Path(r"C:\Users\johnh\Desktop\Datasets\_6_Demographics\US_Census")
+CENSUS_INPUT_DIR = CENSUS_DATA_ROOT / "Input"
+CENSUS_OUTPUT_DIR =  "Census\Output"
